948e5e770dfd7a3e4aaf2ecb8839bf171305bd5e
Felipe Defacio <felipe.defacio@la.logicalis.com>
2022-01-19 20:19:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+
+import { Header } from "./components/Header";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+  );
+}
+
