9428527706cf9a72cd64135a5c24e8a2971d5b50
Felipe Defacio <phelipe2208@gmail.com>
2022-01-24 16:40:56 -0300

    feat: add miragejs as data fetch

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,36 +1,39 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
+export function TransactionTable() {
+  useEffect(() => {
+    api.get("transactions").then((response) => console.log(response.data));
+  }, []);
 
-export function TransactionTable(){
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-            
-            <tbody>
-                <tr>
-                    <td>Desenvolvimento de website</td>
-                    <td className="deposit">R$ 12.800</td>
-                    <td>Desenvolvimento</td>
-                    <td>20/02/2021</td>
-                </tr>
-                <tr>
-                    <td>Aluguel</td>
-                    <td className="withdraw">- R$ 1400</td>
-                    <td>Casa</td>
-                    <td>17/02/2021</td>
-                </tr>
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
 
-            </tbody>
-
-            </table>
-        </Container>
-    );
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.800</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 1400</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }
